6b3cfb393146b50ab8aaa66dc7b08014434f143d
sperezacuna <santiago@perezacuna.com>
2023-11-20 23:27:23 +0100

    Updated relative paths

--- src/utils.py
+++ src/utils.py
@@ -9,16 +9,16 @@ def load_config():
   return config
 
 def inspect_dataframe(dataframe, filename):
-    save_file = "../doc/df_sizes.txt"
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    df = dataframe
-    num_rows, num_columns = df.shape
+  save_file = os.path.join(os.path.dirname(__file__), '../doc/df_sizes.txt')
+  timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+  df = dataframe
+  num_rows, num_columns = df.shape
 
-    with open(save_file, 'a') as f:
-        f.write(f"File analized: {filename}\n")
-        f.write(f"Timestamp: {timestamp}\n")
-        f.write(f"Number of Rows: {num_rows}\n")
-        f.write(f"Number of Columns: {num_columns}\n")
-        f.write("----------------------------\n\n\n")
-    
-    print(f"Row and cols data about {filename} has been added to: {save_file}\n")
+  with open(save_file, 'a') as f:
+    f.write(f"File analized: {os.path.basename(filename)}\n")
+    f.write(f"Timestamp: {timestamp}\n")
+    f.write(f"Number of Rows: {num_rows}\n")
+    f.write(f"Number of Columns: {num_columns}\n")
+    f.write("----------------------------\n\n\n")
+  
+  print(f"Row and cols data about {filename} has been added to: {save_file}\n")
